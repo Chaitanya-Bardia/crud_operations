d3951362f1f146b2ec6be37cfd36aa7a97e56386
Your Name <chaitanyabardia@.com>
2025-06-10 17:23:48 +0530

    added_validation

--- crud/serializers.py
+++ crud/serializers.py
@@ -6,3 +6,8 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = ['id','name', 'surname', 'student_id', 'marks']
+    
+    def validate(self,data):
+        if data['marks'] <= 60:
+            raise serializers.ValidationError("Marks Should be Greater than 60 to create a student")
+        return data
